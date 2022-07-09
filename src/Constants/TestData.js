3050c3d7a310ef01d2ad0b6cf4189f6865d15dd3
Venkata Sivaprasad Reddy Pulagam <pvsivapr@gmail.com>
2022-07-09 16:00:43 +0530

    added test data files

---
+++ src/Constants/TestData.js
@@ -0,0 +1,5 @@
+export const TestData = {
+    "imageURL1": "https://igimages.gumlet.io/telugu/gallery/actress/kaajal/kajal23072021_003.jpg",
+    "imageURL2": "https://www.indiancinemagallery.net/wp-content/photos/celebs/kajal-agarwal/thumbs/thumbs_Kajal-Aggarwal-in-saree-dress-1.jpg",
+    "imageURL3": ""
+}
